a6540cde2f3299d1b62aadd4521d603807cfd10e
Robert Turner <robturneris@gmail.com>
2021-12-19 22:36:33 -0500

    Added styles to Home component

--- components/House.jsx
+++ components/House.jsx
@@ -1,7 +1,23 @@
-export default function House() {
+export default function House(props) {
+  console.log("Propees:", props);
+
   return (
     <div>
-      <h2>House</h2>
+      <img src={props.picture} alt="props.title" width="100%" />
+
+      <p>
+        {props.type} - {props.town}
+      </p>
+      <p>{props.title}</p>
+
+      <style jsx>{`
+        .houses {
+          display: grid;
+          grid-template-columns: 49% 49%;
+          grid-template-rows: 300px 300px;
+          grid-gap: 2%;
+        }
+      `}</style>
     </div>
   )
 }
