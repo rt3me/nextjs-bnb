a96bd1a5acd77eec00f69e67a12663988425568c
Robert Turner <robturneris@gmail.com>
2021-12-19 22:53:32 -0500

    Added id to house data file

--- houses.js
+++ houses.js
@@ -1,11 +1,13 @@
 export default [
   {
+    id: 1,
     picture: "/img/1.jpg",
     type: "Entire house",
     town: "New York",
     title: "Beautiful flat in New York!",
   },
   {
+    id: 2,
     picture: "/img/2.jpg",
     type: "Entire house",
     town: "Amsterdam",
