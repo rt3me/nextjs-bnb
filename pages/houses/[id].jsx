ff12bd78364e4582b783bd49b24f8a82828333a1
Robert Turner <robturneris@gmail.com>
2021-12-19 23:27:55 -0500

    Added getServerSideProps and House comopnent

--- pages/houses/[id].jsx
+++ pages/houses/[id].jsx
@@ -1,7 +1,24 @@
-export default function House() {
+import houses from '../../houses';
+
+export default function House(props) {
   return (
     <div>
-      <p>Test</p>
+      <img src={props.house.picture} alt={props.house.title} width="100%" />
+
+      <p>
+        {props.house.type} - {props.house.town}
+      </p>
+      <p>{props.house.title}</p>
     </div>
   )
+}
+
+export async function getServerSideProps({ query }) {
+  const { id } = query;
+
+  return {
+    props: {
+      house: houses.filter((house) => house.id === parseInt(id))[0],
+    }
+  };
 }
