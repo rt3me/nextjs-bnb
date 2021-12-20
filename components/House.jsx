454f6b51e55e92f1104a954d73040784f725bf30
Robert Turner <robturneris@gmail.com>
2021-12-19 23:02:49 -0500

    Added link to detailed view in House component

--- components/House.jsx
+++ components/House.jsx
@@ -1,14 +1,18 @@
+import Link from 'next/link'
+
 export default function House(props) {
   console.log("Propees:", props);
 
   return (
-    <div>
-      <img src={props.picture} alt="props.title" width="100%" />
+    <Link href="/houses/[id]" as={'/houses/' + props.id}>
+      <a>
+        <img src={props.picture} alt="props.title" width="100%" />
 
-      <p>
-        {props.type} - {props.town}
-      </p>
-      <p>{props.title}</p>
-    </div>
+        <p>
+          {props.type} - {props.town}
+        </p>
+        <p>{props.title}</p>
+      </a>
+    </Link>
   )
 }
