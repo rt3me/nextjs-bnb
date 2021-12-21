9dabd8eb06d7ca1ae42893ff4565b6b347f8d744
Robert Turner <robturneris@gmail.com>
2021-12-20 22:39:22 -0500

    Added Header component to Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 export default function Layout(props) {
   return (
     <React.Fragment>
+      <Header />
       <main>{props.content}</main>
 
       <style jsx>{`
