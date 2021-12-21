ae8ef813e5ea49367ebe861eaefcf4fef72d130c
Robert Turner <robturneris@gmail.com>
2021-12-20 22:38:55 -0500

    Fixed logo Link in Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header(props) {
   return (
     <div className="nav-container">
-      <Link>
+      <Link href="/">
         <a>
           <img src="/img/logo.png" alt="Logo Navigation" />
         </a>
