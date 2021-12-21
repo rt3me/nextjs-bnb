ab0bb032fe729969786c5385d837491e44e5f361
Robert Turner <robturneris@gmail.com>
2021-12-20 21:28:44 -0500

    Added styles to Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,17 @@ export default function Layout(props) {
   return (
     <React.Fragment>
       <main>{props.content}</main>
+
+      <style jsx>{`
+        main {
+          position: relative;
+          max-width: 56em;
+          background-color: white;
+          padding: 2em;
+          margin: 0 auto;
+          box-sizing: border-box;
+        }
+      `}</style>
     </React.Fragment>
   );
 }
