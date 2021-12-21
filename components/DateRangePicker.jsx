eb5456fd07fa9dc0d24b5925a705e69cfb63115d
Robert Turner <robturneris@gmail.com>
2021-12-21 00:35:21 -0500

    Created DateRangePicker component

---
+++ components/DateRangePicker.jsx
@@ -0,0 +1,10 @@
+import DayPickerInput from '../node_modules/react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+
+export default function DateRangePicker() {
+  return (
+    <div>
+      <DayPickerInput />
+    </div>
+  )
+}
