b2ff343173a7021baa7b090768e7f068c4ccdb5c
Robert Turner <robturneris@gmail.com>
2021-12-20 21:07:15 -0500

    Added Layout component to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,29 +3,32 @@ import Head from 'next/head';
 import Image from 'next/image';
 import houses from '../houses.js';
 import House from '../components/House';
+import Layout from '../components/Layout.jsx';
 import styles from '../styles/Home.module.css';
 
+const content = (
+  <React.Fragment>
+    <h2>Places to stay</h2>
+
+    <div className="houses">
+      {
+        houses.map((house, index) => {
+          return <House key={index} {...house} />
+        })
+      }
+    </div>
+
+    <style jsx>{`
+      .houses {
+        display: grid;
+        grid-template-columns: 49% 49%;
+        grid-template-rows: 300px 300px;
+        grid-gap: 2%;
+      }
+    `}</style>
+  </React.Fragment>
+)
+
 export default function Home() {
-  return (
-    <React.Fragment>
-      <h2>Places to stay</h2>
-
-      <div className="houses">
-        {
-          houses.map((house, index) => {
-            return <House key={index} {...house} />
-          })
-        }
-      </div>
-
-      <style jsx>{`
-        .houses {
-          display: grid;
-          grid-template-columns: 49% 49%;
-          grid-template-rows: 300px 300px;
-          grid-gap: 2%;
-        }
-      `}</style>
-    </React.Fragment>
-  );
+  return <Layout content={content} />
 }
