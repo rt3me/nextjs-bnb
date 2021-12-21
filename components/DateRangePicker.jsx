279dcc8a798a1faad2f449a2ab85018326387634
Robert Turner <robturneris@gmail.com>
2021-12-21 01:11:33 -0500

    Added date formatting to date picker

--- components/DateRangePicker.jsx
+++ components/DateRangePicker.jsx
@@ -1,16 +1,39 @@
 import DayPickerInput from '../node_modules/react-day-picker/DayPickerInput';
+import { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import dateFnsFormat from 'date-fns/format';
+import dateFnsParse from 'date-fns/parse';
+
+const parseDate = (str, format, locale) => {
+  const parsed = dateFnsParse(str, format, new Date(), { locale })
+
+  return DateUtils.isDate(parsed) ? parsed : null;
+}
+
+const formatDate = (date, format, locale) => dateFnsFormat(date, format, { locale });
+
+const format = 'MMM dd yyyy';
 
 export default function DateRangePicker() {
   return (
     <div className="date-range-picker-container">
       <div>
         <label>From:</label>
-        <DayPickerInput />
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+        />
       </div>
       <div>
         <label>To:</label>
-        <DayPickerInput />
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+        />
       </div>
 
       <style jsx>
