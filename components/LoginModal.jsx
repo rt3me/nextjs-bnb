1707296ff1856c4aad17b66b1077bf57f119e6d1
Robert Turner <robturneris@gmail.com>
2021-12-22 01:49:27 -0500

    Login modal code and styles

--- components/LoginModal.jsx
+++ components/LoginModal.jsx
@@ -1,3 +1,16 @@
+import React from "react"
+
 export default function LoginModal(props) {
-  return <p>Login Modal</p>
+  return (
+    <React.Fragment>
+      <h2>Log in</h2>
+      <div>
+        <form>
+          <input id="email" type="email" placeholder="Email address" />
+          <input id="password" type="password" placeholder="Password" />
+          <button>Log in</button>
+        </form>
+      </div>
+    </React.Fragment>
+  )
 }
