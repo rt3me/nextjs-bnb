83ecdf57b80beb7b701f9ceefcf72aedd7ad7be8
Robert Turner <robturneris@gmail.com>
2021-12-22 01:54:49 -0500

    Registration modal code

--- components/RegistrationModal.jsx
+++ components/RegistrationModal.jsx
@@ -1,3 +1,21 @@
+import React from "react";
+
 export default function RegistrationModal(props) {
-  return <p>Registration Modal</p>
+  return (
+    <React.Fragment>
+      <h2>Sign up</h2>
+      <div>
+        <form>
+          <input id="email" type="email" placeholder="Email address" />
+          <input id="password" type="password" placeholder="Password" />
+          <input
+            id="passwordconfirmation"
+            type="password"
+            placeholder="Enter password again"
+          />
+          <button>Sign up</button>
+        </form>
+      </div>
+    </React.Fragment>
+  )
 }
